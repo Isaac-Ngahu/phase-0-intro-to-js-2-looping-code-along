bb13581cf3e7aac6636e345d81c48568c0a00008
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2022-11-22 23:13:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const messages=[];
+const event_name="Birthday";
+const names=["Jack","Diane","Fiona"];
+function writeCards(names,event_name){
+    for(let i=0; i<=2; i++){
+        console.log(`Thankyou,${names[i]},for the wonderful,${event_name}gift`);
+        
+    }
+    
+}
+messages.push(writeCards(names,event_name));
+    return messages;
+ 

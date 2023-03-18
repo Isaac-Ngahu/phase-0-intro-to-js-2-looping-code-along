e0dda377092cf8f4a573e05b1f9609ceacb42619
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-03-18 14:03:08 +0300

    done loop

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function writeCards(names,event_name){
         console.log(`Thankyou,${names[i]},for the wonderful,${event_name}gift`);
         
     }
-    
+   
 }
 messages.push(writeCards(names,event_name));
     return messages;
+
+    function countdown(number){
+        while(number>0){
+console.log(number--);
+        }
+    }
  
